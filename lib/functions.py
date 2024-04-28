1bd2762249a8211dcfc0ef68a13abd1cb0602fb7
gamze <gamzekandara@gmail.com>
2024-04-27 21:06:13 -0400

    Adds functions.py

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,25 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
     pass
+    return num1 + num2
+
 
 def halve(number):
     pass
+    if not isinstance(number, str): # checks if number is a string
+        return number / 2  # returns none if line 25 is not true, does not execute the rest. 
